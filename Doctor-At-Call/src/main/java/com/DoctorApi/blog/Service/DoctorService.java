7d8e4903234b34cf4c4a1140750b6ddcbfb54261
Sourabhdalal <87751132+Sourabhdalal@users.noreply.github.com>
2024-02-17 17:54:14 +0530

    updated

---
+++ Doctor-At-Call/src/main/java/com/DoctorApi/blog/Service/DoctorService.java
@@ -0,0 +1,25 @@
+package com.DoctorApi.blog.Service;
+
+import java.util.List;
+
+import com.DoctorApi.blog.Dto.DoctorDto;
+
+
+public interface DoctorService 
+{
+	DoctorDto createDoctor(DoctorDto doctor);
+	  
+	DoctorDto updateDoctor(DoctorDto doctor,Integer doctorId);
+	  
+	DoctorDto getDoctorById(Integer doctorId);
+	  
+	void deleteDoctor(Integer doctorId);
+	  
+	List<DoctorDto> getAllDoctor();
+	
+	DoctorDto logIn(String username,String password);
+	
+	DoctorDto deletePatient(Integer doctorId, Integer patientId);
+	
+	DoctorDto acceptPatient(Integer doctorId, Integer patientId);
+}
