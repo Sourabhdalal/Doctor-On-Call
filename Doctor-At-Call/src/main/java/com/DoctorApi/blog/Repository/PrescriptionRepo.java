4ff8957b09a606c689a56898e42fb06fd7c0f001
Sourabhdalal <87751132+Sourabhdalal@users.noreply.github.com>
2024-02-29 12:43:44 +0530

    Delete Doctor-At-Call directory

--- Doctor-At-Call/src/main/java/com/DoctorApi/blog/Repository/PrescriptionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.DoctorApi.blog.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.DoctorApi.blog.Entity.Prescription;
-
-public interface PrescriptionRepo extends JpaRepository<Prescription,Integer> {
-
-}
--- Doctor-At-Call/src/main/java/com/DoctorApi/blog/Service/PrescriptionService.java
+++
@@ -1,8 +0,0 @@
-package com.DoctorApi.blog.Service;
-
-import com.DoctorApi.blog.Dto.PrescriptionDto;
-
-public interface PrescriptionService 
-{
-	PrescriptionDto generatePrescription(PrescriptionDto ps,Integer patientId,Integer doctorId);
-}
